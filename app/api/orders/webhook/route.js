f3c651e51645ad6aae03b4ffc2d0f23ecb525190
Fathismael93 <fathismael@gmail.com>
2025-09-15 12:36:20 +0300

    Updated add order api

--- app/api/orders/webhook/route.js
+++ app/api/orders/webhook/route.js
@@ -132,7 +132,6 @@ export async function POST(req) {
             {
               $inc: {
                 stock: -item.quantity,
-                sold: item.quantity,
               },
             },
             { session },
