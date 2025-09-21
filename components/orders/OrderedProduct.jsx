95b3eefd2ce2041bd73e6e02434b69561e03046c
Fathismael93 <fathismael@gmail.com>
2025-09-21 12:57:27 +0300

    Updated icons for some components

--- components/orders/OrderedProduct.jsx
+++ components/orders/OrderedProduct.jsx
@@ -44,36 +44,17 @@ const OrderedProduct = memo(({ item }) => {
       {/* Image du produit */}
       <div className="flex-shrink-0">
         <div className="w-20 h-20 rounded-md border border-gray-200 overflow-hidden bg-gray-50 flex items-center justify-center">
-          {image ? (
-            <Image
-              src={image}
-              alt={name}
-              title={`Image de ${name}`}
-              width={80}
-              height={80}
-              className="object-cover w-full h-full"
-              onError={(e) => {
-                e.target.src = '/images/default_product.png';
-              }}
-            />
-          ) : (
-            <div className="text-gray-400">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-10 w-10"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={1.5}
-                  d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                />
-              </svg>
-            </div>
-          )}
+          <Image
+            src={image || '/images/default_product.png'}
+            alt={name}
+            title={`Image de ${name}`}
+            width={80}
+            height={80}
+            className="object-cover w-full h-full"
+            onError={(e) => {
+              e.target.src = '/images/default_product.png';
+            }}
+          />
         </div>
       </div>
 
